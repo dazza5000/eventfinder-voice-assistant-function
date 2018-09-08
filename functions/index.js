a6b5ac459561f0d69643b1e539a2d805e9c76b35
Darran Kelinske <darran.kelinske@gmail.com>
2018-09-08 17:04:36 -0500

    update cloud function

--- functions/index.js
+++ functions/index.js
@@ -3,21 +3,83 @@
 'use strict';
 
 // Import the Dialogflow module from the Actions on Google client library.
-const {dialogflow} = require('actions-on-google');
+const {
+  dialogflow
+} = require('actions-on-google');
 
 // Import the firebase-functions package for deployment.
 const functions = require('firebase-functions');
 
 // Instantiate the Dialogflow client.
-const app = dialogflow({debug: true});
+const app = dialogflow({
+  debug: true
+});
+const https = require('https');
+const host = 'api.meetup.com';
+const httpGET = 'GET';
+const jsonContentType = 'application/json';
 
 // Handle the Dialogflow intent named 'favorite color'.
 // The intent collects a parameter named 'color'.
-app.intent('activity', (conv, {Activity}) => {
-    const luckyNumber = Activity.length;
-    // Respond with the user's lucky number and end the conversation.
-    conv.close('Your lucky number is ' + luckyNumber + ' yeehaw!');
+app.intent('activity', (conv, {
+  Activity
+}) => {
+
+  const luckyNumber = Activity.length;
+  // Respond with the user's lucky number and end the conversation.
+  //conv.close('Your lucky number is ' + luckyNumber + ' yeehaw!');
+  handleEventSearch("hiking");
 });
+
+function handleEventSearch(req) {
+
+  callSearchEvents(req).then((output) => {
+
+    return conv.close('Your lucky number is ' + luckyNumber + ' yeehaw!');
+
+  }).catch((reason) => {
+    return conv.close({
+      'fulfillmentText': reason
+    });
+  });
+}
+
+function callSearchEvents(events) {
+  return new Promise((resolve, reject) => {
+    let path = '2/events?key=62663d4f7b391b7143156918537f7722&group_urlname=ny-tech&sign=true';
+
+    const options = {
+      hostname: host,
+      path: path,
+      method: httpGET,
+      headers: {
+        'Content-Type': jsonContentType
+      }
+    };
+
+    const req = https.request(options, res => {
+      res.setEncoding('utf8');
+      var data = ''
+
+      res.on('data', chunk =>  {
+        data += chunk
+      });
+
+      res.on('end', () => {
+        let responseJson = JSON.parse(data);
+        resolve(responseJson);
+      });
+    });
+
+    req.on('error', (e) => {
+      console.error(`problem with request: ${e.message}`);
+      reject(new Error(`problem with request: ${e.message}`));
+    });
+    req.write(bodyAsJSONString);
+
+    req.end();
+  });
+}
 
 // Set the DialogflowApp object to handle the HTTPS POST request.
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
